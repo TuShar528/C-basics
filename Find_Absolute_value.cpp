4230684e33fa92f77055f0fef3d3604ca0bdb214
Tushar <119734788+TuShar528@users.noreply.github.com>
2024-02-12 22:38:54 +0530

    Update Find_Absolute_value.cpp

--- Find_Absolute_value.cpp
+++ Find_Absolute_value.cpp
@@ -8,12 +8,12 @@ int main(){
     int x;
     cout<<"Enter a integer : ";
     cin>>x;
-    // if (x>0){
-    //     cout<<"Absolute value of x is : "<<x;    //MEANING OF ABOSOLUTE VALUE IS LIKE GIVING ITS 
-    // }
-    // else{
-    //     cout<<"Absolute value of x is : "<<-x;   //POSITIVE VALUE , SAME LIKE MOD IN MATHS..
-    // }
+    if (x>0){
+        cout<<"Absolute value of x is : "<<x;    //MEANING OF ABOSOLUTE VALUE IS LIKE GIVING ITS 
+    }
+    else{
+        cout<<"Absolute value of x is : "<<-x;   //POSITIVE VALUE , SAME LIKE MOD IN MATHS..
+    }
 
 
     //IF YOU WANT TO CHANGE N INTO ITS ABSOLUTE VALUE PERMANANTELY
