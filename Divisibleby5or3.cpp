4b2d280be96d7273253f52d30e7c29d64af749b2
Tushar <119734788+TuShar528@users.noreply.github.com>
2024-02-12 22:37:40 +0530

    Update Divisibleby5or3.cpp

--- Divisibleby5or3.cpp
+++ Divisibleby5or3.cpp
@@ -1,17 +1,17 @@
-// // TAKE POSITIVE INTEGER INPUT AND TELL IF ITS DIVISIBLE BY 5 OR 3
+// TAKE POSITIVE INTEGER INPUT AND TELL IF ITS DIVISIBLE BY 5 OR 3
 
-// // ===================================================================
+// ===================================================================
 
-// #include<iostream>
-// using namespace std;
-// int main(){
-//     int x;
-//     cout<<"Enter a positive number : ";
-//     cin>>x;
-//     if (x%5==0 || x%3==0){                           //or = ||
-//         cout<<x<<" is divisible by  5 or 3";
-//     }
-//     else {
-//         cout<<x<<" is not divisible by  5 or 3";
-//     }
-// }
+#include<iostream>
+using namespace std;
+int main(){
+    int x;
+    cout<<"Enter a positive number : ";
+    cin>>x;
+    if (x%5==0 || x%3==0){                           //or = ||
+        cout<<x<<" is divisible by  5 or 3";
+    }
+    else {
+        cout<<x<<" is not divisible by  5 or 3";
+    }
+}
